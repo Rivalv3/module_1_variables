f6fa5a6e80229d41ea4ad0aa3275d250f59c0b7d
Rivalv3 <167709224+Rivalv3@users.noreply.github.com>
2024-10-21 23:49:19 +0500

    Add files via upload

---
+++ pythonProject/module_1_6.py
@@ -0,0 +1,5 @@
+homework_completed = 12
+hours_spend = 1.5
+course_name = "Python"
+time_per_tasks = hours_spend / homework_completed
+print(course_name, homework_completed, hours_spend, time_per_tasks)
